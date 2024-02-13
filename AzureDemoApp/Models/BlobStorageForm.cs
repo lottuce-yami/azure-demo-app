04201e05d01a5025a8618d2aa84a587b57a45ef3
Lotte <lottuce.yami@gmail.com>
2024-02-13 17:56:57 +0100

    Implement basic form with attribute validation

---
+++ AzureDemoApp/Models/BlobStorageForm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components.Forms;
+using AzureDemoApp.Attributes;
+
+namespace AzureDemoApp.Models;
+
+public class BlobStorageForm
+{
+    [Required]
+    [DocxFile]
+    public IBrowserFile? File { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; }
+}
