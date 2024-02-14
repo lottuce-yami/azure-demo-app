b7c83e9a119778ae23ac0da20db265fc5e16a910
Lotte <lottuce.yami@gmail.com>
2024-02-14 15:53:09 +0100

    Implement Azure blob storage service

--- AzureDemoApp/Program.cs
+++ AzureDemoApp/Program.cs
@@ -1,10 +1,12 @@
 using AzureDemoApp.Components;
+using AzureDemoApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<BlobStorageService>();
 
 builder.Configuration.AddEnvironmentVariables(prefix: "azuredemo_");
 
